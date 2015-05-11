8c2e2646526aa53a2842ad423caf4d45e13ae423
gapoorva <gapoorva@umich.edu>
2015-05-11 00:16:23 -0400

    Basic Login functions complete
    Got it to the point where we can log people in and create users. At
    time of writing, have not uploaded this on the server. Forgot the reds
    lololol

--- js/loginscript.js
+++ js/loginscript.js
@@ -1,22 +1,46 @@
-
-$("signerup").submit( function (e) {signup(e.target);});
-function signup (e) {
 Parse.initialize("CRiHeLQl3IT2amq2a9kHCvAwhkmwNn2VEMVaKN5N", "mFcmOluxGuB0QhM3F0BsQW361QI5iPMdVMa8ynaj");
+var resp;
+var errorAlert = "<div class='alert alert-danger'><a href='#'' class='close' data-dismiss='alert' aria-label='close'>&times;</a>";
+$("#signerup").submit(
+  function (e) {
+    e.preventDefault();
+   //  console.log(e);
+   //  var info = $("#signerup").serialize();
+   //  console.log(e.currentTarget[0].value);
+   //  console.log(e.currentTarget[1].value);
+   //  console.log(e.currentTarget[2].value);
 
-  var user = new Parse.User();
-      var resp;
-      user.set("username", e.name.value);
-      user.set("password", e.password.value);
-      user.set("email", e.email.value);
+    var user = new Parse.User();
+       
+        user.set("username", e.currentTarget[0].value);
+        user.set("email", e.currentTarget[1].value);
+        user.set("password", e.currentTarget[2].value);
 
-      user.signUp(null, {
-        success: function (user) { 
+        user.signUp(null, {
+          success: function (user) { 
+            resp = user;
+           // console.log("Did it!");
+           // alert("did it");
+          },
+          error: function (user, error) {
+            console.log(error.code, error.message);
+            $("#signerup h1").after(errorAlert+error.message+"</div>")
+          }
+        });
+});
+
+$("#logerin").submit(
+    function (e) {
+      e.preventDefault();
+      Parse.User.logIn(e.currentTarget[0].value, e.currentTarget[1].value, {
+        success: function (user) {
           resp = user;
-          console.log("Did it!");
-          alert("did it");
+          console.log("success!");
+          window.location.href ="http://www.apoorvagupta.com";//need to add new page here
         },
         error: function (user, error) {
-          console.log(error.code, error.message);
+            console.log(error.code, error.message);
+            $("#logerin h1").after(errorAlert+error.message+"</div>")
         }
-      });
-}
+    });
+});
