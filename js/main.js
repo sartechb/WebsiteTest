8d83eeeb4936ccf32f06d8c2dd6248c5f3d797a7
gapoorva <gapoorva@umich.edu>
2015-06-13 23:14:26 -0700

    Manual Merge completed

--- js/main.js
+++ js/main.js
@@ -1,18 +1,86 @@
-$( '.filter-class' ).on( 'click', 'input:checkbox', function () {
-  if((this).value === "showall")
-  {
-    $(".post").show();    
-    $(".filtered").removeClass("filtered");
-  }
-  else{
-    $(".post").hide();
-    $(".filtered").removeClass("filtered");
-    $(this).parent().addClass("filtered");
-    $(document.getElementById((this).value)).show(); 
-  }
-});
+// $( '.filter-class' ).on( 'click', 'input:checkbox', function () {
+//   if((this).value === "showall")
+//   {
+//     $(".post").show();    
+//     $(".filtered").removeClass("filtered");
+//   }
+//   else{
+//     $(".post").hide();
+//     $(".filtered").removeClass("filtered");
+//     $(this).parent().addClass("filtered");
+//     $(document.getElementById((this).value)).show(); 
+//   }
+// });
+
+/*
+ * The app variable will be a global application variable.
+ * One can use the app variable to save debug data and
+ * view it at the console. It also helps the app maintain
+ * state for uses like filtering.
+ */
+var app = {};
+
+/*
+ * How this works: we initialize important page elements
+ * like the name, uni, etc in this function. We add filter
+ * data to the app variable based on information we get from
+ * parse. It's set up so each filter has it's own array of post
+ * IDs that it corresponds to. This makes it easy to hide
+ * and show the correct posts, and all this info is 
+ * maintained and manipulated internally
+ */
+
+  //building filters
+  app.filters = {};
+
+  //replace this part with info taken from parse:
+  //TODO: PARSE INTEGRATION HERE
+  app.filters["EECS_370"] = [];
+  app.filters["EECS_475"] = [];
+  app.filters["EECS_485"] = [];
+  app.filters["LING_341"] = [];
+
+  //building post mappings
+  //replace this part with info taken from parse:
+  //TODO: PARSE INTEGRATION HERE
+  app.filters["EECS_370"].push("eecs370");
+  app.filters["EECS_475"].push("eecs475");
+  app.filters["EECS_485"].push("eecs485");
+  app.filters["LING_341"].push("ling341");
+
+  //console.log(app);
 
 
+$(".filter-class div").on("click", function (e) {filterToggle(e);});
+$("div.showall").on("click", function (e) {
+  $(".post.on").removeClass("on");
+  $(".post").not(".template-post").show();
+  $(".filtered").removeClass("filtered");
+ });
+ 
+function filterToggle(e) {
+  var filter = $(e.target).parent().attr("id");
+  //console.log(e.target);
+  filterToggleHelper(filter);
+  $(e.target).parent().toggleClass("filtered");
+}
+
+
+function filterToggleHelper (filter) {
+  console.log(filter);
+  var a = app.filters[filter];
+  console.log(a[0]);
+  for(var i=0; i < a.length; ++i) 
+    if($("#"+a[i]).hasClass("on"))
+      $("#"+a[i]).removeClass("on");
+    else
+      $("#"+a[i]).addClass("on");
+  $(".post").not(".template-post").show(200);
+  if($(".post.on").length != 0)
+    $(".post").not(".on").hide();
+}
+
+// Controls dropdown code
 $('#dropdown').on('click', function(){
   $('#sidebar').slideToggle(200);
   if($('#content').hasClass('col-xs-10'))
@@ -42,7 +110,7 @@ $('#dropdown').on('click', function(){
 });
 
 
-
+//allows for separate scrolling or side bar and body
 $('body').scroll(function() { 
   $('#sidebar').css('top', $(this).scrollTop());
 });
@@ -67,7 +135,7 @@ $('#new-post').submit(function (e) {
   e.preventDefault();
   console.log(e);
 
-  var errorAlert = "<div class='alert alert-danger'><a href='#'' class='close' data-dismiss='alert' aria-label='close'>&times;</a>";
+  var errorAlert = "<div class='alert alert-danger col-xs-8 col-xs-offset-2'><a href='#'' class='close' data-dismiss='alert' aria-label='close'>&times;</a>";
   var input_length = $("#createpost input, #createpost textarea").length;
   for(var i = 0; i < input_length; ++i) {//iterate through all inputs
     if(e.currentTarget[i].value == "") {//if any are empty
