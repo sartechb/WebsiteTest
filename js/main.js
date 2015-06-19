5fb3a5f6749c6d6af0df466c81babb21f626893e
eleventhirtyeight <guitar.gamer.geek@gmail.com>
2015-06-18 21:45:10 -0400

    Mobile
    Postfeed optimized, show and hide sidebar on hamburger menu

--- js/main.js
+++ js/main.js
@@ -83,15 +83,9 @@ function filterToggleHelper (filter) {
 
 // Controls dropdown code
 $('#dropdown').on('click', function(){
+  if($(window).width() > 768)
+  {
   $('#sidebar').slideToggle(200);
-  if($('#content').hasClass('col-xs-10'))
-  {
-   $('#content').switchClass( "col-xs-10", "col-xs-12", 200, "easeInOutQuad" );
-  }
-  else if($('#content').hasClass('col-xs-12'))
-  {
-    $('#content').switchClass( "col-xs-12", "col-xs-10", 200, "easeInOutQuad" ); 
-  }
   if($('#content').hasClass('col-md-10'))
   {
    $('#content').switchClass( "col-md-10", "col-md-12", 200, "easeInOutQuad" );
@@ -100,14 +94,24 @@ $('#dropdown').on('click', function(){
   {
     $('#content').switchClass( "col-md-12", "col-md-10", 200, "easeInOutQuad" ); 
   }
-  if($('#content').hasClass('col-sm-10'))
+  
+}
+else
+{
+
+  
+
+  
+  if($('#sidebar').hasClass('hidden-xs'))
   {
-   $('#content').switchClass( "col-sm-10", "col-sm-12", 200, "easeInOutQuad" );
+    $('#content').slideToggle(200);
+    $('#sidebar').delay(200).switchClass( "hidden-xs", "col-xs-12", 0, "swing" ); 
   }
-  else if($('#content').hasClass('col-sm-12')) 
-  {
-    $('#content').switchClass( "col-sm-12", "col-sm-10", 200, "easeInOutQuad" ); 
+  else{
+    $('#sidebar').switchClass( "col-xs-12", "hidden-xs", 200, "swing" ); 
+    $('#content').delay(200).slideToggle(200);
   }
+}
 });
 
 
