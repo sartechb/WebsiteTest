74050ce8d38dea1cbe97cc6ab62fb19b6947f2aa
Preeti Mohan <guitar.gamer.geek@gmail.com>
2015-07-08 14:23:01 -0400

    fixed so width dynamically changes on tablet

--- js/sidebar.js
+++ js/sidebar.js
@@ -11,7 +11,7 @@ $('#dropdown').on('click', function(){
   {
     $('#content').switchClass( "col-md-12", "col-md-10", 200, "easeInOutQuad" ); 
   }
-  else if($('#content').hasClass('col-sm-10'))
+  if($('#content').hasClass('col-sm-10'))
   {
    $('#content').switchClass( "col-sm-10", "col-sm-12", 200, "easeInOutQuad" );
   }
