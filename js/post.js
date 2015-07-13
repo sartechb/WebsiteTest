ac43e44055566cacf543524028c6deb8ca7b46b1
gapoorva <gapoorva@umich.edu>
2015-07-12 19:16:06 -0700

    Detail location in parenthesis

--- js/post.js
+++ js/post.js
@@ -166,7 +166,7 @@ function createPost(post, set, append, postBefore, glow) {
   to_insert.find("#title img").attr("src", "assets/"+post.authorPic);
   to_insert.find("#postDetails h7").html(post.author+" | <i class=\"fa fa-clock-o\"></i> "+prettyTime(new Date(post.time)));
   to_insert.find("#posttext h5").html(post.content);
-  to_insert.find("#lowerDetails h7").html("<i class='fa fa-book'></i> "+post.classString+" | <i class='fa fa-location-arrow'></i> "+post.location+" "+post.detailLocation);
+  to_insert.find("#lowerDetails h7").html("<i class='fa fa-book'></i> "+post.classString+" | <i class='fa fa-location-arrow'></i> "+post.location+" ("+post.detailLocation+")");
   to_insert.find("#following h7").html(post.memberCount+(post.memberCount==1?" member":" members")+" joined so far");
   to_insert.attr("id", post.postId);
   to_insert.removeClass("template-post");
