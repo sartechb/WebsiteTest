ba005b45f6e4589ec355d3c850426334e850f53a
gapoorva <gapoorva@umich.edu>
2015-08-05 01:20:27 -0700

    Notify text bug

--- js/detail.js
+++ js/detail.js
@@ -413,7 +413,7 @@ function runUpdates() {
         }
         app.util.lastComment = null;
         order.doOp(fixComments);
-      } else 
+      } else if($(".comment-holder .no-comments").length == 0)
         $("#postholder .comment-holder").append("<div class='col-xs-10 col-xs-offset-1 no-comments'><h4>No comments yet. Leave one above!<h4></div>");
     }, error: function(error) {console.log(error);}
   });
