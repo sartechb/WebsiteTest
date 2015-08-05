08bfd335d2f6448d31c0353974bfe6909d826757
gapoorva <gapoorva@umich.edu>
2015-08-05 01:12:01 -0700

    Login error

--- js/loginscript.js
+++ js/loginscript.js
@@ -14,7 +14,7 @@ Parse.Cloud.run("functionNameHere", {data:here}, {
 });
 */
 
-if(Parse.User)
+if(Parse.User.current())
   window.location.href = "http://sartechb.github.io/WebsiteTest/index.html";
 
 
