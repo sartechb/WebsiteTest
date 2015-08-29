1fa159dcbadd0141ce71fdcdff477e304bb7627f
gapoorva <gapoorva@umich.edu>
2015-08-29 10:44:23 -0400

    Revert "Merge branch 'WobettoStage' into gh-pages"
    This reverts commit 2a46d1d1f742766611cf7d6da9f19cc71b2f0c0f, reversing
    changes made to baaf94bcdc1a09c642ab9005d82979b3ad1e850f.

--- js/security.js
+++ js/security.js
@@ -1,2 +1,1 @@
-Parse.initialize("OIxxnQzpNgYpAMGuPjTjpsQaMVHsIKqUuAf28doO", "3FC4ECnN2TUrD5tFWdQIBAnyyOm9BAQ4O1GeVeIs");
-
+Parse.initialize("MQvT5Bq6CsU34IQBfop8fPEJLOsLybDgDMBRdFhM", "HNFXaE7aCayggyI8hyUvbk5kG2sWXH2FpMirSNyC");
