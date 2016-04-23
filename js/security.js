799127390b5732a6c075fbb0adfbfde5f88f46f3
Sarthak Bhandari <sarthak1657@gmail.com>
2016-04-23 15:50:04 -0400

    Update security.js
    Moving gh-pages to production

--- js/security.js
+++ js/security.js
@@ -1,2 +1,2 @@
-Parse.initialize("OIxxnQzpNgYpAMGuPjTjpsQaMVHsIKqUuAf28doO", "3FC4ECnN2TUrD5tFWdQIBAnyyOm9BAQ4O1GeVeIs");
+Parse.initialize("hkAI1bVuw3lirFps0pvRPyBGFSa7TjKJOEjYXbkF", "DtCJuQjyXYA9rMTASQc55f4ik0ixCg6KHk5iiok1");
 
